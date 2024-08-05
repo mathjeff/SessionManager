4443fd1d2910e9c4e4e00c4a81564c52379a2466
Jeffry Gaston <gastoj3@gmail.com>
2024-08-05 15:05:20 +0800

    fix for first initialization when the window dir doesn't exist

--- SessionManager.sh
+++ SessionManager.sh
@@ -79,7 +79,7 @@ shift || true
 
 if [ "$command" == "newWindowId" ]; then
   when="$(date +%s)"
-  numberOfWindows="$(ls $windowsDir | wc -l)"
+  numberOfWindows="$(ls $windowsDir 2>/dev/null | wc -l)"
   echo "${when}_${numberOfWindows}"
   exit
 fi
